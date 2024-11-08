ceee6a0ec4e0ca3b6769883f10b056ce03a614bf
Scaleupapp <144357944+Scaleupapp@users.noreply.github.com>
2024-11-08 08:38:06 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const api = axios.create({
- // baseURL: 'https://nirpekshnandan.com',
- baseURL: 'http://localhost:3001',
+  baseURL: 'https://nirpekshnandan.com',
+ //baseURL: 'http://localhost:3001',
 });
 
 api.interceptors.request.use(
